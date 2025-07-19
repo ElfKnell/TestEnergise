967d3ebfeb5b28665eb111570df3d9c991773364
Andrii Kyrychenko <akyrych84@gmail.com>
2025-07-19 09:07:39 +0200

    Initial Commit

---
+++ TestEnergise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestEnergise
+//
+//  Created by Andrii Kyrychenko on 19/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
